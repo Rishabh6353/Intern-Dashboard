8198a0221098506d425b24cbdc2c89bac52c3933
Rishabh6353 <rishabhoop@gmail.com>
2025-08-05 01:24:00 +0530

    Updated env files

--- server/src/app.js
+++ server/src/app.js
@@ -6,15 +6,17 @@ const app = express();
 dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
 const mongoose = require('mongoose');
+const CLIENT_URL = process.env.CLIENT_URL; 
+
+
 
 app.use(cors({
-  origin: ['https://intern-dashboard.onrender.com'], 
+  origin: CLIENT_URL, 
   credentials: true
 }));
-app.use(express.json())
+
 app.use('/api/users', userRoutes);
 app.set("PORT",(process.env.PORT || 3000))
-app.use(cors())
 app.use(express.json({limit:"40kb"}))
 app.use(express.urlencoded({limit:"40kb", extended:"true"}))
 
